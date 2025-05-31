698349f8bd1d41ef3c5a78f655ceefd3ba2e7a24
PogSmok <lukaszprus.job@gmail.com>
2025-05-31 13:14:44 +0200

    Restructured

---
+++ server/entry.js
@@ -0,0 +1,10 @@
+class entryData {
+    // energyCost in KWh
+    constructor(energyCost, roofSpace) {
+        this.energyCost = energyCost;
+        this.roofSpace = roofSpace;
+
+    }
+}
+
+module.exports = Entry_data;
