a99a32f16f073073f170fc252de1cfce8b704102
PogSmok <lukaszprus.job@gmail.com>
2025-05-31 13:52:27 +0200

    update

--- server/entry.js
+++ server/entry.js
@@ -1,11 +1,22 @@
 class entryData {
-    // energyCost in KWh
-    constructor(energyCost, roofSpace) {
-        this.energyCost = energyCost;
-        this.roofSpace = roofSpace;
-
+    constructor(consumption, buyPrice, pvSize, pyProduction, sellPrice, pvCostPerKw, autoconsumption, priceIncrease) {
+        this.consumption = consumption;
+        this.buyPrice = buyPrice;
+        this.pvSize = pvSize;
+        this.pyProduction = pyProduction;
+        this.sellPrice = sellPrice;
+        this.pvCostPerKw = pvCostPerKw;
+        this.autoconsumption = autoconsumption;
+        this.priceIncrease = priceIncrease;
     }
 }
 
-module.exports = Entry_data;
+
+function import_data(req) {
+    const {consumption, buyPrice, pvSize, pvProduction, sellPrice, pvCostPerKw, autoconsumption, priceIncrease} = req.body;
+    return entryData(consumption, buyPrice, pvSize, pvProduction, sellPrice, pvCostPerKw, autoconsumption, priceIncrease);
+}
+
+
+module.exports = {Entry_data, import_data};
 
