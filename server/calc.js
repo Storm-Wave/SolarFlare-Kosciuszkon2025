340ec1f36d913f382e834fa942c62eab77f000c8
BeesyBumbleBee <sz.oleskiewicz@gmail.com>
2025-05-31 14:46:38 +0200

    Modified calc.js

--- server/calc.js
+++ server/calc.js
@@ -1,18 +1,28 @@
-const savings = {
-    yearlySavings: [],
-    yearlyCosts: [],
+class CalcData  {
+    constructor(yearlySavings, yearlyCostsNoPV, yearlyCostsPV) {
+        this.yearlySavings = yearlySavings;
+        this.yearlyCostsNoPV = yearlyCostsNoPV;
+        this.yearlyCostsPV = yearlyCostsPV;
+    }
 }
 
-let calculateYearlyReturn = function(years, entryData) {
-    let savings = Object.entries(savings)
+const years = 25;
+
+let calculateYearlyReturn = function(entryData) {
     let prevYearSellPrice = entryData.sellPrice;
     let prevYearBuyPrice = entryData.buyPrice;
     let cumulatedSavings = 0;
+    let cumulatedCostPV = 0;
+    let cumulatedCostNoPV = 0;
+    let yearlySavings = [];
+    let yearlyCostsPV = [];
+    let yearlyCostsNoPV = [];
     for (y = 0; y < years; y++) {
         let yearSellPrice = prevYearSellPrice * (1 + entryData.priceIncrease);
         let yearBuyPrice = prevYearBuyPrice * (1 + entryData.priceIncrease);
         prevYearSellPrice = yearSellPrice;
         prevYearBuyPrice = yearBuyPrice;
+
         let costNoPV = yearBuyPrice * entryData.consumption;
         let energyProduced = entryData.pvProduction * entryData.pvSize;
         let autoconsumption = entryData.autoconsumption * energyProduced;
@@ -20,8 +30,17 @@ let calculateYearlyReturn = function(years, entryData) {
         let energyConsumedWithPV = entryData.consumption - autoconsumption;
         let profit = yearSellPrice * energySold;
         let costPV = yearBuyPrice * energyConsumedWithPV - profit;
+
         cumulatedSavings += costNoPV - costPV;
-        savings.yearlySavings.push(cumulatedSavings);
+        cumulatedCostPV += costPV;
+        cumulatedCostNoPV += costNoPV;
+
+        yearlySavings.push(cumulatedSavings);
+        yearlyCostsNoPV.push(costNoPV);
+        yearlyCostsPV.push(costPV);
     }
-    return savings;
+    return new CalcData(
+        yearlySavings,
+        yearlyCostsNoPV,
+        yearlyCostsPV);
 }
