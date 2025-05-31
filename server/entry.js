a390872f22bd6340cf1fb0e7ff5b764170456fa0
PogSmok <lukaszprus.job@gmail.com>
2025-05-31 13:37:38 +0200

    base

---
+++ server/entry.js
@@ -0,0 +1,11 @@
+class entryData {
+    // energyCost in KWh
+    constructor(energyCost, roofSpace) {
+        this.energyCost = energyCost;
+        this.roofSpace = roofSpace;
+
+    }
+}
+
+module.exports = Entry_data;
+
--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Route to handle form submission
+app.post('/submit', (req, res) => {
+  // Access the form data from req.body
+  const { name, email } = req.body;
+  console.log('Received data:', { name, email });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
 
